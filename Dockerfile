a80a216cb499647b8b5e1951d6fe75f523bfb82d
Abdullah Habib <abdullahhabib.86@gmail.com>
2024-04-24 01:42:08 +0500

    version 1

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM python:3.12-slim
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Copy the requirements file to the working directory
+COPY requirements.txt .
+
+# Install the Python dependencies
+RUN pip install -r requirements.txt
+
+# Copy the rest of the application code into the container
+COPY . .
+
+# Expose the port your app runs on
+EXPOSE 8501
+
+# Define the command to run your application
+CMD ["streamlit", "run", "app.py"]
--- app.py
+++ app.py
@@ -1,20 +1,46 @@
 import streamlit as st
+import os
+from dotenv import load_dotenv
+import google.generativeai as genai
+import shutil
+from tempfile import NamedTemporaryFile
 
+# Load environment variables from .env file
+load_dotenv()
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY') 
 
-def transcribe_audio(audio_file):
-    return "hello"
+genai.configure(api_key=GOOGLE_API_KEY)
 
-def process_prompt(system_prompt, audio_transcript):
-    prompt = f"System Prompt: {system_prompt}\nAudio Transcript: {audio_transcript}\nResponse:" 
-    return prompt
+def process_audio(system_prompt, audio_file_path):
+    your_file = genai.upload_file(path=audio_file_path)
+    model = genai.GenerativeModel('models/gemini-1.5-pro-latest')
+    response = model.generate_content([system_prompt, your_file])
+    print(response.text)
+    return response.text
 
 # Streamlit App Structure
 st.title("Audio Prompt Processor")
 
-system_prompt = st.text_input("Enter your system prompt:")
+system_prompt = st.text_area("Enter your system prompt:", height=100)  # Multi-line input
 audio_file = st.file_uploader("Upload an audio file:")
 
-if system_prompt and audio_file:
-    audio_transcript = transcribe_audio(audio_file)
-    output_text = process_prompt(system_prompt, audio_transcript)
-    st.text_area("Output", output_text) 
+if system_prompt and audio_file and st.button("Submit"):
+    # Save the uploaded file locally
+    # Save the uploaded file
+    os.makedirs("audiofiles", exist_ok=True)
+
+    # Determine the save path of the file
+    filename = audio_file.name
+    save_path = os.path.join("audiofiles", filename)
+
+    # Save the uploaded file
+    with open(save_path, "wb") as f:
+        f.write(audio_file.getvalue()) 
+
+
+    # # Process the audio using the saved path
+    output_text = process_audio(system_prompt, save_path)
+    st.markdown(output_text) 
+
+    # # Clean up the temporary file
+    os.remove(save_path)
